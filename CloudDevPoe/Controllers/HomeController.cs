2188e8c982d1db68234b9db7398cf2b64c4fb7dc
JamesWestonKnox <ST10048826@vcconnect.edu.za>
2024-03-19 16:45:01 +0200

    Revert "Added My Work page"
    This reverts commit 8a08d5909f7f45715f63a8018ffcba0678f6ac68.

--- CloudDevPoe/Controllers/HomeController.cs
+++ CloudDevPoe/Controllers/HomeController.cs
@@ -28,10 +28,6 @@ namespace CloudDevPoe.Controllers
             return View();
         }
 
-        public IActionResult MyWork()
-        {
-            return View();
-        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

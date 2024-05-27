d161a6324c9222718110a46bd5088675793b1f3f
JamesWestonKnox <ST10048826@vcconnect.edu.za>
2024-05-27 15:15:48 +0200

    Finished styling and layout changes as well as fixed loginFailed view

--- CloudDevPoe/Controllers/HomeController.cs
+++ CloudDevPoe/Controllers/HomeController.cs
@@ -54,6 +54,8 @@ namespace CloudDevPoe.Controllers
 
 
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
